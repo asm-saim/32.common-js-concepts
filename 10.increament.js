6f80cac11d18d641656db405438b0b17b7729750
asm-saim <saim777m@gmail.com>
2026-06-15 19:01:53 +0600

    pre/post increment, pre/post decrement

--- 10.increament.js
+++ 10.increament.js
@@ -3,3 +3,6 @@ console.log(counter = counter + 1)
 console.log(counter += 1)
 console.log(counter++) //post increment(previous value 2, print 2 than increase 1 counter= 3)
 console.log(++counter) //pre  increment (previous 3 + pre increment 1 , so, 4)
+
+console.log(counter--) //post decrement
+console.log(--counter) //pre decrement 
