a240037fa8086fc80112f88ab7548a0dc51f0d47
asm-saim <saim777m@gmail.com>
2026-06-15 18:46:00 +0600

    pre increment, post increment implememted

---
+++ 10.increament.js
@@ -0,0 +1,5 @@
+let counter = 0;
+console.log(counter = counter + 1)
+console.log(counter += 1)
+console.log(counter++) //post increment(previous value 2, print 2 than increase 1 counter= 3)
+console.log(++counter) //pre  increment (previous 3 + pre increment 1 , so, 4)
